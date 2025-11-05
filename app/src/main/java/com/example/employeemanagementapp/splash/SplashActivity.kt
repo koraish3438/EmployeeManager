6d49599f1c992c5b33af3cbca9b739996b781967
koraish3438 <koraishmdkajamall38@gmail.com>
2025-11-06 00:04:28 +0600

    Third commit

--- app/src/main/java/com/example/employeemanagementapp/splash/SplashActivity.kt
+++ app/src/main/java/com/example/employeemanagementapp/splash/SplashActivity.kt
@@ -1,10 +1,12 @@
 package com.example.employeemanagementapp.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.employeemanagementapp.registration.WelcomeActivity
 import com.example.employeemanagementapp.R
 
 class SplashActivity : AppCompatActivity() {
@@ -12,10 +14,11 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500) // 1.5 seconds delay
     }
 }
