170334afd4e81f7d2473a82d19cc381592cf6255
koraish3438 <koraishmdkajamall38@gmail.com>
2025-11-08 12:07:44 +0600

    10th commit

--- app/src/main/java/com/example/employeemanagementapp/data/Employee.kt
+++ app/src/main/java/com/example/employeemanagementapp/data/Employee.kt
@@ -3,7 +3,6 @@ package com.example.employeemanagementapp.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "employees")
 data class Employee(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
